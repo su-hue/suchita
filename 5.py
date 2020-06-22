74c150958984f447f8ebb76beeeb089232593312
su-hue <66860502+su-hue@users.noreply.github.com>
2020-06-22 13:11:12 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,15 @@
+run1=int(input("Runs of first batsman "))
+run2=int(input("Runs of second batsman "))
+run3=int(input("Runs of third batsman "))
+str1=run1*1.67
+str2=run2*1.67
+str3=run3*1.67
+print("strike rate of first batsman ",str1)
+print("strike rate of second batsman ",str2)
+print("strike rate of third batsman ",str3)
+print("run scored if player 1 played 60 balls more ",run1*2)
+print("run scored if player 2 played 60 balls more ",run2*2)
+print("run scored if player 3 played 60 balls more ",run3*2)
+print("first player could hit ",run1//6," six")
+print("second player could hit ",run2//6," six")
+print("third player could hit ",run3//6," six")
